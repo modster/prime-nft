83401e3cf8f491bd3ffbc9942e5748e4d34400a1
E. M. Greeff <emgreeff@datadash.cloud>
2023-08-05 07:13:38 -0600

    counter 1

--- test/HelloWorld.ts
+++ test/HelloWorld.ts
@@ -8,5 +8,9 @@ describe("HELLO WORLD", function () {
     //2. delploy our contract
     //3. call our contract to test
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
+
+    expect(await hello.hello()).to.equal("Hello, World");
   });
 });
