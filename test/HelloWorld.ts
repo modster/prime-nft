2f3c17f5b2924c1e6a32c326727d83bbf6677c8f
E. M. Greeff <emgreeff@datadash.cloud>
2023-08-05 03:35:51 -0600

    tests

--- test/HelloWorld.ts
+++ test/HelloWorld.ts
@@ -1,1 +1,12 @@
-import
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("HELLO WORLD", function () {
+  it("SHOULD GET THE HELLO WORLD", async function () {
+    //1. setup
+    //2. delploy our contract
+    //3. call our contract to test
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+  });
+});
